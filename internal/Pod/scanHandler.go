9a5f9edef741715289373ab2af1a30254922376b
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2026-04-20 23:02:58 +0530

    YoraSys: Try to print out data from registry
    Signed-off-by: DragonEmperor9480 <amruteshnaregal1234@gmail.com>

--- internal/Pod/scanHandler.go
+++ internal/Pod/scanHandler.go
@@ -15,7 +15,26 @@ func ScanAnamolies(registryPath string) {
 		fmt.Println(err)
 	}
 
-	fmt.Println("Registry:", reg)
+	fmt.Printf("Registry: %s | Version: %v | Platform: %s", reg.Schema.Name, reg.Schema.Version, reg.Platform)
+
+	for _, cache := range reg.Caches {
+		fmt.Printf("ID: %d\n", cache.ID)
+		fmt.Printf("Name: %s\n", cache.Name)
+		fmt.Printf("Category: %s\n", cache.Category)
+		fmt.Printf("Description: %s\n", cache.Description)
+
+		if len(cache.Paths) == 0 {
+			fmt.Println("Paths: none")
+			continue
+		}
+
+		fmt.Println("Paths:")
+		for j, p := range cache.Paths {
+			fmt.Printf("  %d. %s\n", j+1, p)
+		}
+
+		fmt.Println()
+	}
 
 }
 
@@ -28,15 +47,15 @@ func loadRegistry(path string) (schematics.Registry, error) {
 	var reg schematics.Registry
 
 	if err := yaml.Unmarshal(val, &reg); err != nil {
-		return schematics.Registry{}, errors.New("Failed to Parse yaml")
+		return reg, errors.New("Failed to Parse yaml")
 
 	}
 
 	if reg.Platform == "" {
-		return schematics.Registry{}, errors.New("Invalid OS")
+		return reg, errors.New("Invalid OS")
 	}
 	if len(reg.Caches) == 0 {
-		return schematics.Registry{}, errors.New("No cache data found")
+		return reg, errors.New("No cache data found")
 
 	}
 
